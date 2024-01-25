a3643c0af1b8149db884486c49ba3e3258aba4d3
YangGuangcan <guangcanyang@yeah.net>
2024-01-25 23:38:59 +0800

    1
    1

--- plot_gantt_chart.py
+++ plot_gantt_chart.py
@@ -10,7 +10,7 @@ from src import *
 # file = "example.csv"  # 甘特图数据文件
 # file_save = "%s/%s" % (save_dir, file[:-4])  # 保存的甘特图名称
 """Algorithm result"""
-save, instance, exp = "GA_JSP", "ft06", "1"
+save, instance, exp = "GA_FSP", "car1", "1"
 file_dir = "./%s/%s/GanttChartReal" % (save, instance)  # 甘特图数据文件所在目录
 save_dir = "./%s/%s/GanttChartPngHtml" % (save, instance)  # 生成的甘特图保存目录
 # file_dir2 = "./%s/%s/GanttChartReal2" % (save, instance)  # 甘特图数据文件所在目录
@@ -22,10 +22,10 @@ file_save = "%s/%s" % (save_dir, file[:-4])  # 保存的甘特图名称
 """===================================================================================="""
 a = GanttChart("%s/%s" % (file_dir, file))  # 调用甘特图生成类
 a.gantt_chart_png(
-    filename=file_save, fig_width=9, fig_height=5, random_colors=False, lang=1,
+    filename=file_save, fig_width=9, fig_height=5, random_colors=False, lang=0,
     dpi=200, height=0.8, scale_more=12, x_step=a.schedule.makespan // 10,
     y_based=0, text_rotation=0,
-    with_operation=True, with_start_end=False, key_block=True, show=False
+    with_operation=True, with_start_end=False, key_block=False, show=False
 )  # 绘制png格式的甘特图
 # a.schedule.time_unit = 60  # 设置加工时间单位
 # a.gantt_chart_html(date="2020 7 6", filename=file_save, show=False, lang=0)  # 绘制html格式的甘特图
